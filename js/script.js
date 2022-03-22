40bfb7be1ac31c7f2ab6c5fd9f9c5a19836c6cb4
Grace Were <100326395+GraceWere@users.noreply.github.com>
2022-03-22 12:35:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Responsive navigation menu//
 const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
+  navigationBtn.classList.toggle("active");
 });
