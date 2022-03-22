28642256a05a7fc8b00af6d15e031c2dd2d4befd
Grace Were <100326395+GraceWere@users.noreply.github.com>
2022-03-22 17:20:01 +0300

    update landing page

--- js/script.js
+++ js/script.js
@@ -9,12 +9,25 @@ menuBtn.addEventListener("click", () => {
 
 //Video slider navigation//
 const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
 
 var sliderNav = function (manual) {
   btns.forEach((btn) => {
     btn.classList.remove("active");
   });
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
   btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
 };
 
 btns.forEach((btn, i) => {
