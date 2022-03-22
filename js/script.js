07f118d6c37b28057657a328db14200b664a0911
Grace Were <100326395+GraceWere@users.noreply.github.com>
2022-03-22 16:07:42 +0300

    update slider navigation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigationBtn.classList.toggle("active");
 });
+
+//Video slider navigation//
+const btns = document.querySelectorAll(".nav-btn");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
