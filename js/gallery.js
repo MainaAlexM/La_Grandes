21090dd590f3f0e795e759391d09ca8d075db9b6
Evaline Wangu <evalinemacharia99@gmail.com>
2022-03-22 18:50:12 +0300

    Add background color on the body

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+$(".D").hover(function(){
+    $(this).animate({opacity:'1'});
+  },
+  function(){
+    $(this).animate({opacity:'0'});
+  });
+  
+$(".B").hover(function(){
+$(this).animate({opacity:'1'});
+},
+function(){
+$(this).animate({opacity:'0'});
+    })
+}); 
